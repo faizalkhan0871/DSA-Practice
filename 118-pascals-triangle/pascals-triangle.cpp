25263b5ff3fde04ff422518b8c37df402077ca8d
faizalkhan0871 <faizalkhan7582@gmail.com>
2026-04-23 15:55:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (29.96%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -4,12 +4,12 @@ public:
         vector<vector<int>>results;
         for(int i = 0 ; i < numRows ; i++){
             vector<int>row;
-            long long val = 1;
+            long long ans = 1;
             row.push_back(1);
             for(int col = 1 ; col<=i ; col++){
-                val = val *(i-col+1);
-                val = val/col;
-                row.push_back(val);
+                ans = ans *(i-col+1);
+                ans = ans/col;
+                row.push_back(ans);
                 
             }
             results.push_back(row);
