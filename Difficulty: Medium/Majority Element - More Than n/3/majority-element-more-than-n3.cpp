d7437879fcd757468894697f306a8d9cdd8351b8
Faizal Khan <faizalkhan7582@gmail.com>
2026-08-02 17:02:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.cpp
+++ Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.cpp
@@ -1,24 +1,27 @@
 class Solution {
-  public:
+public:
     vector<int> findMajority(vector<int>& arr) {
+
         int el1 = 0, el2 = 0;
         int count1 = 0, count2 = 0;
         int n = arr.size();
 
-        for(int i = 0; i < n; i++){
-            if(arr[i] == el1) {
+        // Candidate Selection
+        for(int i = 0; i < n; i++) {
+
+            if(count1 == 0 && arr[i] != el2) {
+                el1 = arr[i];
+                count1 = 1;
+            }
+            else if(count2 == 0 && arr[i] != el1) {
+                el2 = arr[i];
+                count2 = 1;
+            }
+            else if(arr[i] == el1) {
                 count1++;
             }
             else if(arr[i] == el2) {
                 count2++;
-            }
-            else if(count1 == 0) {
-                el1 = arr[i];
-                count1 = 1;
-            }
-            else if(count2 == 0) {
-                el2 = arr[i];
-                count2 = 1;
             }
             else {
                 count1--;
@@ -26,19 +29,23 @@ class Solution {
             }
         }
 
+        // Verification
         int count3 = 0, count4 = 0;
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; i++) {
             if(arr[i] == el1) count3++;
             if(arr[i] == el2) count4++;
         }
 
         vector<int> ans;
 
-        if(count3 > n/3) ans.push_back(el1);
-        if(el2 != el1 && count4 > n/3) ans.push_back(el2);
-        
-        sort(ans.begin(),ans.end());
+        if(count3 > n/3)
+            ans.push_back(el1);
+
+        if(el1 != el2 && count4 > n/3)
+            ans.push_back(el2);
+
+        sort(ans.begin(), ans.end());   // GFG generally expects sorted output
 
         return ans;
     }
