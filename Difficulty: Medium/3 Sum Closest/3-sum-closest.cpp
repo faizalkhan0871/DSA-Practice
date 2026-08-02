5ed4cd76e4f12a48497435f33c184a458ede7091
Faizal Khan <faizalkhan7582@gmail.com>
2026-08-02 17:14:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/3 Sum Closest/3-sum-closest.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int closest3Sum(vector<int> &arr, int target) {
+
+        int n = arr.size();
+
+        sort(arr.begin(), arr.end());
+
+        int closestSum = arr[0] + arr[1] + arr[2];
+
+        for (int i = 0; i < n - 2; i++) {
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+
+                int sum = arr[i] + arr[left] + arr[right];
+
+                // Better answer mila
+                if (abs(sum - target) < abs(closestSum - target)) {
+                    closestSum = sum;
+                }
+
+                // GFG tie case
+                else if (abs(sum - target) == abs(closestSum - target)
+                         && sum > closestSum) {
+                    closestSum = sum;
+                }
+
+                if (sum < target) {
+                    left++;
+                }
+                else if (sum > target) {
+                    right--;
+                }
+                else {
+                    return sum;   // Exact target mil gaya
+                }
+            }
+        }
+
+        return closestSum;
+    }
+};
