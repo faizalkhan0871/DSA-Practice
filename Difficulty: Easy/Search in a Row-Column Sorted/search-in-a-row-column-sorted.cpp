4d95add25dd0d1d060ce58ef53f0d195b05c7488
Faizal Khan <faizalkhan7582@gmail.com>
2026-08-10 16:44:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Search in a Row-Column Sorted/search-in-a-row-column-sorted.cpp
+++ Difficulty: Easy/Search in a Row-Column Sorted/search-in-a-row-column-sorted.cpp
@@ -1,25 +1,22 @@
-// User function template for C++
 class Solution {
   public:
-    bool matSearch(vector<vector<int>> &mat, int x) {
-        int n = mat.size();
-        int m = mat[0].size();
-
+    bool matSearch(vector<vector<int>> &arr, int x) {
+        
+        int n = arr.size();
+        int m = arr[0].size();
         int row = 0;
-        int col = m - 1;
-
-        while(row < n && col >= 0){
-            
-            if(mat[row][col] == x)
-                return true;
-
-            else if(mat[row][col] > x)
-                col--;
-
-            else
+        int col = m-1;
+        while(row<n && col>=0){
+            if(arr[row][col]<x){
                 row++;
+            }
+            else if(arr[row][col]>x){
+                col--;
+            }
+            else{
+                return true;
+            }
         }
-
         return false;
     }
 };
