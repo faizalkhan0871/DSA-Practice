8cd4e013c2e36905acd7a2e0b8654aa7ef52b48f
Faizal Khan <faizalkhan7582@gmail.com>
2026-09-14 16:06:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Longest Consecutive 1's/longest-consecutive-1s.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int maxConsecutiveOnes(int n) {
+    int count = 0;
+    int maxCount = 0;
+
+        while(n > 0) {
+
+                    if(n & 1) {
+                        count++;
+                        maxCount = max(maxCount, count);
+                    }
+                    else {
+                        count = 0;
+                    }
+
+                    n = n >> 1;
+                }
+
+                return maxCount;
+            }
+        };
+        
+    
+
